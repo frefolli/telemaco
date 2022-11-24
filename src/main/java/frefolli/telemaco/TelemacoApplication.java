bef5428a5269de07213f89b3a1af74144e90149c
frefolli <f.refolli@campus.unimib.it>
2022-11-24 21:50:01 +0100

    first commit

---
+++ src/main/java/frefolli/telemaco/TelemacoApplication.java
@@ -0,0 +1,13 @@
+package frefolli.telemaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemacoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelemacoApplication.class, args);
+	}
+
+}
